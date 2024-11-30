0e31b9d62afc5134c96da61842a6a93aacc00059
fabras91 <fconcha@unsa.edu.pe>
2024-11-30 08:16:18 -0500

    modificano main y creando primer modulo de authors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthorsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
