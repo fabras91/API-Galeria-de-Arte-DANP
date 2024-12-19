98c211324f24629907fb9da3dfa85f210c5df3fc
fabras91 <fconcha@unsa.edu.pe>
2024-12-18 21:38:20 -0500

    añadiendo post de pictures

--- src/pictures/pictures.service.ts
+++ src/pictures/pictures.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePictureDto } from './dto/create-picture.dto';
 import { UpdatePictureDto } from './dto/update-picture.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Picture } from './entities/picture.entity';
+import { Repository } from 'typeorm';
+import { CreateAuthorDto } from 'src/authors/dto/create-author.dto';
 
 @Injectable()
 export class PicturesService {
-  create(createPictureDto: CreatePictureDto) {
-    return 'This action adds a new picture';
+  constructor(
+    @InjectRepository(Picture)
+    private pictureRepository: Repository <Picture>,
+  ){}
+
+  async create(createPictureDto: CreatePictureDto) {
+    const picture = this.pictureRepository.create(createPictureDto);
+    return await this.pictureRepository.save(picture);
   }
 
   findAll() {
